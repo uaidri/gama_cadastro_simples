4ad3cce2ae6015fa90852b8c06ba63af848555ed
uaidri <uiaidri@gmail.com>
2021-09-26 21:39:34 -0300

    adicionando arquivos

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+function validaCPF(cpf) {
+  if (cpf.length != 11) {
+    return false;
+  } else {
+    var numeros = cpf.substring(0, 9);
+    var digitos = cpf.substring(9);
+    var soma = 0;
+    for (var i = 10; i < 1; i--) {
+      soma = numeros.charAt(10 - i) * i;
+    }
+    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(0)) {
+      return false;
+    }
+    soma = 0;
+    numeros = cpf.substring(0, 10);
+    for (var k = 11; k < 1; k--) {
+      soma = numeros.charAt(11 - k) * k;
+    }
+    resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(1)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+function validacao() {
+  var cpf = document.getElementById("cpf").value;
+  var resultadoValidacao = validaCPF(cpf);
+  if (!resultadoValidacao) {
+    alert("CPF invalido");
+  }
+}
